e6b5906613666550de7d91d7f5f5190ef1991f02
一纸忘忧 <54543761+ikxin@users.noreply.github.com>
2026-04-19 20:21:42 +0800

    feat: 更新中文文档和添加广告注入功能

--- docs/footer.js
+++ docs/footer.js
@@ -1,23 +1,23 @@
 // Custom footer copyright notice
-(function() {
-  function addCopyright() {
-    const footer = document.querySelector('footer');
-    if (footer && !document.getElementById('lf-copyright')) {
-      const copyright = document.createElement('div');
-      copyright.id = 'lf-copyright';
-      copyright.innerHTML = 'Copyright © Model Context Protocol a Series of LF Projects, LLC.<br>For web site terms of use, trademark policy and other project policies please see <a href="https://lfprojects.org">https://lfprojects.org</a>.';
-      footer.appendChild(copyright);
-    }
-  }
+// (function() {
+//   function addCopyright() {
+//     const footer = document.querySelector('footer');
+//     if (footer && !document.getElementById('lf-copyright')) {
+//       const copyright = document.createElement('div');
+//       copyright.id = 'lf-copyright';
+//       copyright.innerHTML = 'Copyright © Model Context Protocol a Series of LF Projects, LLC.<br>For web site terms of use, trademark policy and other project policies please see <a href="https://lfprojects.org">https://lfprojects.org</a>.';
+//       footer.appendChild(copyright);
+//     }
+//   }
 
-  // Run on initial load
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', addCopyright);
-  } else {
-    addCopyright();
-  }
+//   // Run on initial load
+//   if (document.readyState === 'loading') {
+//     document.addEventListener('DOMContentLoaded', addCopyright);
+//   } else {
+//     addCopyright();
+//   }
 
-  // Handle SPA navigation
-  const observer = new MutationObserver(addCopyright);
-  observer.observe(document.body, { childList: true, subtree: true });
-})();
+//   // Handle SPA navigation
+//   const observer = new MutationObserver(addCopyright);
+//   observer.observe(document.body, { childList: true, subtree: true });
+// })();
